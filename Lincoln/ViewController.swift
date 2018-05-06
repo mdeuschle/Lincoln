2f60b0b65f6778c6b3fadba455d9fd1362653a30
Matt Deuschle <mdeuschle@gmail.com>
2018-05-06 12:03:07 -0700

    World

--- Lincoln/ViewController.swift
+++ Lincoln/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello America")
+        print("Hello World")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
